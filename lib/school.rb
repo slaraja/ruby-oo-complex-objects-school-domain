cd92c5615f83103c4cdfd970e5dea45fdb224c57
Stephanie La Raja <stephanielaraja@gmail.com>
2021-01-31 17:02:53 -0500

    commit tree 3f57ba6b41b67346acf9d7393a41cc7e54c3e85d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School 
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name=name
+        @roster = {}
+    end
+
+    def add_student(student, level)
+        roster[level] ||= []
+        roster[level] << student
+    end
+
+    def grade(level)
+        roster.find do |x, y| 
+          if x == level
+            return y 
+          end 
+        end 
+    end 
+
+    def sort 
+        new_hash = {}
+        roster.each do |a, b| 
+          new_hash[a] = b.sort 
+        end 
+        new_hash
+    end 
+end
